d68a70b45c01d3d4b35185f241beb2b2bee61655
mariantoniettaangelini <164540376+mariantoniettaangelini@users.noreply.github.com>
2024-05-23 16:42:44 +0200

    ESERCITAZIONE S9-L4

--- src/app/Components/post-details/post-details.component.ts
+++ src/app/Components/post-details/post-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { iPost } from '../../Models/post';
+import { iJsonImp } from '../../Models/iJsonImp';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './post-details.component.scss'
 })
 export class PostDetailsComponent {
+  postArr:iPost[] = []
+  postInDetail!: iPost;
 
+  constructor(
+    private route:ActivatedRoute
+  ) {
+}
+
+ngOnInit() {
+  this.route.params.subscribe((params:any)=> {
+    let dettaglio = this.postArr.find(d => d.id === params.id)
+    if(dettaglio) {
+      this.postInDetail = dettaglio
+      console.log(this.postInDetail)
+    }
+  })
+}
+
+  async getPosts() {
+    const response = await fetch ('../../../assets/db.json')
+    const details = <iJsonImp> await response.json()
+    this.postArr = details.posts
+  }
 }
